51cb3556d305dde1569b156038e9b3ce0c815a0c
Sorab <sohorhab.azizdel@outlook.com>
2023-02-02 01:19:45 +1100

    - Merge New Changes from Fork - Added Store Image

--- src/dist/background.js
+++ src/dist/background.js
@@ -1,15 +1,15 @@
 // On installation show html page and set applicationIsOn to true, + settings
 browser.runtime.onInstalled.addListener(async () => {
     await browser.tabs.create({url: "popup/install.html"});
-    await browser.storage.local.set({AUTOYT_applicationIsOn: true});
-    await browser.storage.local.set({AUTOYT_filterByMaxLength: "none"});
-    await browser.storage.local.set({AUTOYT_filterByMinLength: "none"});
-    await browser.storage.local.set({AUTOYT_amountOfPlaysToSkip: 1});
-    await browser.storage.local.set({AUTOYT_scrollOnComments: false});
-    await browser.storage.local.set({AUTOYT_shortCutKeys: ["shift", "s"]});
-    await browser.storage.local.set({AUTOYT_shortCutInteractKeys: ["shift", "f"]});
+    await browser.storage.local.set({applicationIsOn: true});
+    await browser.storage.local.set({filterByMaxLength: "none"});
+    await browser.storage.local.set({filterByMinLength: "none"});
+    await browser.storage.local.set({amountOfPlaysToSkip: 1});
+    await browser.storage.local.set({scrollOnComments: false});
+    await browser.storage.local.set({shortCutKeys: ["shift", "s"]});
+    await browser.storage.local.set({shortCutInteractKeys: ["shift", "f"]});
     await browser.storage.local.set({
-        AUTOYT_filteredAuthors: ["SoRadGaming"],
+        filteredAuthors: ["SoRadGaming"],
     });
 });
 browser.runtime.onUpdateAvailable.addListener(() => {
